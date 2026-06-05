0bd04e3dfb6ad33ba3236a6259f0d955de1dd186
Danush <danulfc0703@gmail.com>
2026-06-05 20:24:09 +0000

    Update allowed hosts for deployment

--- thegreenstep/settings.py
+++ thegreenstep/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-9-$jc$p6#yzz62)(kgofxz@y)s6nia$!9%-$1oohs6c0=@pe5$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
