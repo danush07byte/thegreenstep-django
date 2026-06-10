5c14452677d1cea553f366e77653d3275feb4bdd
Danush <danulfc0703@gmail.com>
2026-06-10 20:43:03 +0000

    Final lecturer corrections completed

--- campaign/admin.py
+++ campaign/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Volunteer
 
-admin.site.register(Volunteer)
+admin.site.site_header = "The Green Step Admin Panel"
+admin.site.site_title = "SDG 13 Admin"
+admin.site.index_title = "Climate Action Volunteer Management"
+
+@admin.register(Volunteer)
+class VolunteerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'interest')
+    search_fields = ('name', 'email', 'interest')
+    list_filter = ('interest',)
+    ordering = ('name',)
